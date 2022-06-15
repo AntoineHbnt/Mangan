20dfd06108bac9195ebe3454472e3cd048470e2e
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-06-15 19:47:01 +0200

    init project style

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,17 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react"],
   rules: {
+    "linebreak-style": ["error", "windows"],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
   },
 };
